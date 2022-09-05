a06d26705e675a5b0af4d2464aec24ca40e7bc61
Spencer Christiansen <jc.spencer92@gmail.com>
2022-09-05 12:38:38 -0600

    Fix: typing Enter in inputs was toggling first neighbor

--- src/components/Emulator/RenderState/Configuring/index.tsx
+++ src/components/Emulator/RenderState/Configuring/index.tsx
@@ -25,57 +25,55 @@ const toggleNeighbor =
 
 const Configuring: React.FC<Props> = ({ store, state }) => (
   <div>
-    <form>
-      <label className={`block`}>
-        <span className={`block text-sm font-medium text-slate-700`}>States</span>
-        <input
-          type="number"
-          min={store.minStates}
-          max={store.maxStates.map(String).getOrElseValue('')}
-          step="1"
-          value={store.states.map(String).getOrElseValue(state.states)}
-          onChange={(e) => store.setStates(e.target.value)}
-        />
-        {store.states.cata({
-          Ok: () => <></>,
-          Err: (e) => <span>{JSON.stringify(e)}</span>,
-        })}
-      </label>
-      <div>
-        <span className={`block text-sm font-medium text-slate-700`}>Neighbors</span>
-        {[-3, -2, -1, 0, 1, 2, 3].map((i) => (
-          <button
-            className={`${
-              isNeighborSelected(state, i) ? 'border-green-300' : 'border-slate-300'
-            } border-2 w-7 h-7 m-0.5 rounded`}
-            key={i}
-            onClick={toggleNeighbor(store, state, i)}
-          >
-            {i}
-          </button>
-        ))}
-        {store.neighbors.cata({
-          Ok: () => <></>,
-          Err: (e) => <span>{JSON.stringify(e)}</span>,
-        })}
-      </div>
+    <label className={`block`}>
+      <span className={`block text-sm font-medium text-slate-700`}>States</span>
+      <input
+        type="number"
+        min={store.minStates}
+        max={store.maxStates.map(String).getOrElseValue('')}
+        step="1"
+        value={store.states.map(String).getOrElseValue(state.states)}
+        onChange={(e) => store.setStates(e.target.value)}
+      />
+      {store.states.cata({
+        Ok: () => <></>,
+        Err: (e) => <span>{JSON.stringify(e)}</span>,
+      })}
+    </label>
+    <div>
+      <span className={`block text-sm font-medium text-slate-700`}>Neighbors</span>
+      {[-3, -2, -1, 0, 1, 2, 3].map((i) => (
+        <button
+          className={`${
+            isNeighborSelected(state, i) ? 'border-green-300' : 'border-slate-300'
+          } border-2 w-7 h-7 m-0.5 rounded`}
+          key={i}
+          onClick={toggleNeighbor(store, state, i)}
+        >
+          {i}
+        </button>
+      ))}
+      {store.neighbors.cata({
+        Ok: () => <></>,
+        Err: (e) => <span>{JSON.stringify(e)}</span>,
+      })}
+    </div>
 
-      <label className={`block`}>
-        <span className={`block text-sm font-medium text-slate-700`}>Rule ID</span>
-        <input
-          type="number"
-          min={store.minRuleId}
-          max={store.maxRuleId.map(String).getOrElseValue('')}
-          step="1"
-          value={store.ruleId.map(String).getOrElseValue(state.ruleId)}
-          onChange={(e) => store.setRuleId(e.target.value)}
-        />
-        {store.ruleId.cata({
-          Ok: () => <></>,
-          Err: (e) => <span>{JSON.stringify(e)}</span>,
-        })}
-      </label>
-    </form>
+    <label className={`block`}>
+      <span className={`block text-sm font-medium text-slate-700`}>Rule ID</span>
+      <input
+        type="number"
+        min={store.minRuleId}
+        max={store.maxRuleId.map(String).getOrElseValue('')}
+        step="1"
+        value={store.ruleId.map(String).getOrElseValue(state.ruleId)}
+        onChange={(e) => store.setRuleId(e.target.value)}
+      />
+      {store.ruleId.cata({
+        Ok: () => <></>,
+        Err: (e) => <span>{JSON.stringify(e)}</span>,
+      })}
+    </label>
     <div className={`transition-all ease-in-out delay-150 duration-300`}>
       <History emulatorStore={store} />
     </div>
