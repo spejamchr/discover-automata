df751b65fb00d3440ba3a33bbccf4e82a63a0d64
Spencer Christiansen <jc.spencer92@gmail.com>
2022-09-06 21:43:51 -0600

    Simplify coloring to improve performance

--- src/utils/ColorPicker/index.tsx
+++ src/utils/ColorPicker/index.tsx
@@ -1,23 +1,19 @@
 import Store from '../../components/Emulator/Store';
-import { serialize } from '../CellularAutomata';
 import { State } from '../CellularAutomata/Types';
-import { hasher } from '../StrHash';
 
 export type ColorPicker = (state: State) => [string, string];
 
 export const makeColorPicker = (store: Store): ColorPicker => {
-  const { automata } = store;
+  const { states } = store.automata;
   const minHueStepSize = 30;
-  const hueStepSize = Math.max(minHueStepSize, 90 / automata.states);
-  const hueOffset = hasher(serialize(automata)) % 360;
-  const hues = [...Array(automata.states)].map((_, i) => (i * hueStepSize + hueOffset) % 360);
+  const hueStepSize = Math.max(minHueStepSize, 90 / states);
+  const hueOffset = 0;
+  const hues = [...Array(states)].map((_, i) => (i * hueStepSize + hueOffset) % 360);
 
   const buffer = 10;
-  const lStepSize = (100 - 2 * buffer) / (automata.states - 1);
+  const lStepSize = (100 - 2 * buffer) / (states - 1);
   const ls =
-    automata.states === 1
-      ? [50]
-      : [...Array(automata.states)].map((_, i) => i * lStepSize + buffer).reverse();
+    states === 1 ? [50] : [...Array(states)].map((_, i) => i * lStepSize + buffer).reverse();
 
   const colors = hues.map(
     (h, i) => `hsl(${h}, ${store.state.displayInColor ? '90%' : '0%'}, ${ls[i]}%)`,
@@ -25,8 +21,8 @@ export const makeColorPicker = (store: Store): ColorPicker => {
 
   if (store.state.showStateLabels) {
     const textColors = ls.map((l) => (l >= 50 ? 'black' : 'white'));
-    return (state: State) => [colors[state % automata.states], textColors[state % automata.states]];
+    return (state: State) => [colors[state % states], textColors[state % states]];
   } else {
-    return (state: State) => [colors[state % automata.states], 'rgba(0, 0, 0, 0)'];
+    return (state: State) => [colors[state % states], 'rgba(0, 0, 0, 0)'];
   }
 };
