abb44e9d670d0291103d41421356463774d455c3
Spencer Christiansen <jc.spencer92@gmail.com>
2022-09-18 14:10:25 -0600

    Limit width of Rule label: reduce accidental clicks
    Before the label was stretched across the width of the page, no matter
    how narrow the rule input field was. This made it easy to accidentally
    focus on the Rule input when clicking what seemed like an empty part of
    the page.

--- src/components/Emulator/RenderState/ConfigureRuleId/index.tsx
+++ src/components/Emulator/RenderState/ConfigureRuleId/index.tsx
@@ -13,38 +13,39 @@ interface Props {
 
 const ConfigureRuleId: React.FC<Props> = ({ store }) => (
   <span>
-    <label className={`block`}>
+    <label className={`block`} htmlFor="rule">
       <span className={`block text-sm font-medium`}>
         <T kind="Rule" /> <RuleIdSize store={store} />
       </span>
-      <span className={`flex max-w-full items-center transition-all duration-500 ease-in-out`}>
-        <input
-          className={`min-w-[248px] font-mono`}
-          style={{
-            width: `${Math.max(
-              store.maxRuleId.map((r) => r.toString().length).getOrElseValue(12) + 6,
-              store.userRuleId.length + 6,
-            )}ch`,
-          }}
-          type={store.maxRuleId
-            .andThen(whenLER(BigInt(Number.MAX_SAFE_INTEGER)))
-            .map(always('number'))
-            .getOrElseValue('text')}
-          min={String(store.minRuleId)}
-          max={store.maxRuleId.map(String).getOrElseValue('')}
-          step="1"
-          value={store.userRuleId}
-          onChange={(e) => store.setRuleId(e.target.value)}
-        />
-        <Button onClick={store.randomizeRules}>
-          <T kind="Randomize" />
-        </Button>
-      </span>
-      {store.ruleId.cata({
-        Ok: () => <></>,
-        Err: (e) => <span>{JSON.stringify(e)}</span>,
-      })}
     </label>
+    <span className={`flex max-w-full items-center transition-all duration-500 ease-in-out`}>
+      <input
+        id="rule"
+        className={`min-w-[248px] font-mono`}
+        style={{
+          width: `${Math.max(
+            store.maxRuleId.map((r) => r.toString().length).getOrElseValue(12) + 6,
+            store.userRuleId.length + 6,
+          )}ch`,
+        }}
+        type={store.maxRuleId
+          .andThen(whenLER(BigInt(Number.MAX_SAFE_INTEGER)))
+          .map(always('number'))
+          .getOrElseValue('text')}
+        min={String(store.minRuleId)}
+        max={store.maxRuleId.map(String).getOrElseValue('')}
+        step="1"
+        value={store.userRuleId}
+        onChange={(e) => store.setRuleId(e.target.value)}
+      />
+      <Button onClick={store.randomizeRules}>
+        <T kind="Randomize" />
+      </Button>
+    </span>
+    {store.ruleId.cata({
+      Ok: () => <></>,
+      Err: (e) => <span>{JSON.stringify(e)}</span>,
+    })}
   </span>
 );
 
