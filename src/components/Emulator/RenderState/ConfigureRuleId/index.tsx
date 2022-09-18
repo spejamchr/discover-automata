94793341f43dcbe2c2ec286f94373c2464a697bd
Spencer Christiansen <jc.spencer92@gmail.com>
2022-09-18 00:20:52 -0600

    Prevent the Rule input from wrapping

--- src/components/Emulator/RenderState/ConfigureRuleId/index.tsx
+++ src/components/Emulator/RenderState/ConfigureRuleId/index.tsx
@@ -17,28 +17,29 @@ const ConfigureRuleId: React.FC<Props> = ({ store }) => (
       <span className={`block text-sm font-medium`}>
         <T kind="Rule" /> <RuleIdSize store={store} />
       </span>
-      <input
-        className={`min-w-[248px] max-w-full font-mono transition-all duration-500 ease-in-out`}
-        style={{
-          width: `${Math.max(
-            24,
-            store.maxRuleId.map((r) => r.toString().length).getOrElseValue(12) + 6,
-            store.userRuleId.length + 6,
-          )}ch`,
-        }}
-        type={store.maxRuleId
-          .andThen(whenLER(BigInt(Number.MAX_SAFE_INTEGER)))
-          .map(always('number'))
-          .getOrElseValue('text')}
-        min={String(store.minRuleId)}
-        max={store.maxRuleId.map(String).getOrElseValue('')}
-        step="1"
-        value={store.userRuleId}
-        onChange={(e) => store.setRuleId(e.target.value)}
-      />
-      <Button onClick={store.randomizeRules}>
-        <T kind="Randomize" />
-      </Button>
+      <span className={`flex max-w-full items-center transition-all duration-500 ease-in-out`}>
+        <input
+          className={`min-w-[248px] font-mono`}
+          style={{
+            width: `${Math.max(
+              store.maxRuleId.map((r) => r.toString().length).getOrElseValue(12) + 6,
+              store.userRuleId.length + 6,
+            )}ch`,
+          }}
+          type={store.maxRuleId
+            .andThen(whenLER(BigInt(Number.MAX_SAFE_INTEGER)))
+            .map(always('number'))
+            .getOrElseValue('text')}
+          min={String(store.minRuleId)}
+          max={store.maxRuleId.map(String).getOrElseValue('')}
+          step="1"
+          value={store.userRuleId}
+          onChange={(e) => store.setRuleId(e.target.value)}
+        />
+        <Button onClick={store.randomizeRules}>
+          <T kind="Randomize" />
+        </Button>
+      </span>
       {store.ruleId.cata({
         Ok: () => <></>,
         Err: (e) => <span>{JSON.stringify(e)}</span>,
