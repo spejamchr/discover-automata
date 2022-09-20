1664267672bee118c9c5a86e0d0d28e86525659f
Spencer Christiansen <jc.spencer92@gmail.com>
2022-09-19 23:52:16 -0600

    Fix range bug when max is 0
    In JS 0 is falsy.

--- src/utils/Range/index.ts
+++ src/utils/Range/index.ts
@@ -1,7 +1,7 @@
 export function range(min: number, max: number): Array<number>;
 export function range(count: number): Array<number>;
 export function range(minOrCount: number, max?: number) {
-  if (max) {
+  if (typeof max === 'number') {
     return [...Array(max - minOrCount + 1)].map((_, i) => i + minOrCount);
   } else {
     return [...Array(minOrCount)].map((_, i) => i);
