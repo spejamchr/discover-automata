354077ff8fbf3a06f329de21ea9ecdeb91054209
Spencer Christiansen <jc.spencer92@gmail.com>
2022-09-22 00:06:40 -0600

    Link from emulation to explanation

--- src/pages/[locale]/emulate.tsx
+++ src/pages/[locale]/emulate.tsx
@@ -1,5 +1,6 @@
 import type { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import Head from 'next/head';
+import Link from 'next/link';
 import React from 'react';
 import Emulator from '../../components/Emulator';
 import T from '../../utils/Locales/T';
@@ -33,9 +34,16 @@ const Emulate: NextPage<Props> = ({ locale }) => {
         </TranslatorContext.Consumer>
 
         <main>
-          <h1 className={`pb-2 text-lg`}>
-            <T kind={'One-dimensional Cellular Automata'} />
-          </h1>
+          <span className="flex flex-wrap items-end justify-between">
+            <h1 className={`pb-2 pr-4 text-lg`}>
+              <T kind={'One-dimensional Cellular Automata'} />
+            </h1>
+            <Link href={`/${locale}`}>
+              <a className="pb-2 font-medium underline">
+                <T kind="What's this all about?" />
+              </a>
+            </Link>
+          </span>
           <Emulator />
         </main>
       </div>
