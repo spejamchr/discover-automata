60b4b6397c164a92cb8452b02b2d12cfe1bba296
Spencer Christiansen <jc.spencer92@gmail.com>
2022-09-25 01:11:42 -0600

    Give the language picker access to the current locale

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import TitleBar from '../components/TitleBar';
+import { currentLocalePath } from '../utils/LocalePath';
+import { defaultLocale } from '../utils/Locales/Types';
+import LocaleContextProvider from '../utils/Locales/LocaleContextProvider';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const locale = currentLocalePath()
+    .map(({ locale }) => locale)
+    .getOrElseValue(defaultLocale);
   return (
     <>
-      <TitleBar />
+      <LocaleContextProvider locale={locale}>
+        <TitleBar />
+      </LocaleContextProvider>
       <Component {...pageProps} />
     </>
   );
