0df612ef986e85d57687d9cdab3443b4ebb47d33
Spencer Christiansen <jc.spencer92@gmail.com>
2022-10-03 21:54:16 -0600

    Make the colors of the locale picker consistent
    And prevent it from blending into the title bar.

--- src/components/LanguagePicker/Picker/index.tsx
+++ src/components/LanguagePicker/Picker/index.tsx
@@ -11,9 +11,7 @@ const Picker: React.FC<Props> = () => {
   return (
     <Menu.Items>
       <span className="relative">
-        <div
-          className={'absolute top-full right-0 w-fit rounded bg-slate-200 p-1 dark:bg-slate-600'}
-        >
+        <div className={'absolute top-8 right-0 w-fit rounded bg-slate-200 p-1 dark:bg-slate-700'}>
           <LocaleContext.Consumer>
             {({ locale: currentLocale }) =>
               sortedLocales.map(({ nativeLocale, locale }) =>
@@ -33,7 +31,7 @@ const Picker: React.FC<Props> = () => {
                           locale={locale}
                           className={clsx(
                             'flex w-full justify-between rounded px-4 py-2 text-left text-sm leading-5 no-underline',
-                            { 'bg-slate-300 dark:bg-slate-500': active },
+                            { 'bg-slate-300 dark:bg-slate-600': active },
                           )}
                           href={currentLocalePath()
                             .map(withLocale(locale))
