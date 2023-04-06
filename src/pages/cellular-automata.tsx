849a6b1e8ec8a04ed67df874183ce1d75b9e501e
Spencer Christiansen <jc.spencer92@gmail.com>
2023-04-06 10:56:19 -0600

    Fix import

--- src/pages/cellular-automata.tsx
+++ src/pages/cellular-automata.tsx
@@ -1,9 +1,11 @@
 import { GetStaticProps } from 'next';
 import { defaultLocale } from '../utils/Locales/Types';
-import CellularAutomata, { Props, getStaticProps as getStaticPropsImpl } from './[locale]/CellularAutomata';
+import CellularAutomata, {
+  Props,
+  getStaticProps as getStaticPropsImpl,
+} from './[locale]/cellular-automata';
 
 export default CellularAutomata;
 
 export const getStaticProps: GetStaticProps<Props> = () =>
   getStaticPropsImpl({ params: { locale: defaultLocale } });
-
